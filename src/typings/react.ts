95540ea0dd0ece51a45ddca7047e56963f25a98c
Yanislav Igonin <yanislavdabest@gmail.com>
2023-02-21 20:51:20 +0200

    update FCWC type

--- src/typings/react.ts
+++ src/typings/react.ts
@@ -1,4 +1,4 @@
-import { type FC, type ReactNode } from "react";
+import { type FC, type PropsWithChildren } from 'react';
 
-// Function Component With Children
-export type FCWC<T> = FC<T & {children: ReactNode}>;
+// Functional Component With Children
+export type FCWC<P = Record<string, unknown>> = FC<PropsWithChildren<P>>;
