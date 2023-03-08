5a509bfe3edeb5dd1e736e1c6e6668138413ce70
Yanislav Igonin <yanislavdabest@gmail.com>
2023-03-09 00:54:08 +0200

    wip

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,18 +1,43 @@
-import { Input, Layout } from "@/components"
+import { Input, Layout, LineChart } from "@/components"
+import { api } from "@/utils/api";
 import Head from "next/head"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+
+const useSearch = (search: string) => api.trends
+  .search.useQuery({ query: search }, { refetchOnWindowFocus: false });
+
+const useDebounce = (value: string, delay: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      console.log('debounce', value);
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
 
 const Search = () => {
   const [search, setSearch] = useState('')
+
+  const debouncedSearch = useDebounce(search, 500)
+  const { data } = useSearch(debouncedSearch)
 
   return <Layout>
     <Head>
       <title>Search</title>
     </Head>
-    <div className="flex justify-center">
+    <div className="flex flex-col justify-center">
       <div className="w-1/2">
         <Input value={search} onChange={({ currentTarget: { value } }) => setSearch(value)} />
       </div>
+      {data && <LineChart data={data.data} xKey="start" yKey="tweet_count" />}
     </div>
   </Layout>
 }
